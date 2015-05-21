c1751ca0258732b414f2d059c68a5aaa89aaf517
haibei_zheng <seashell@cienet.com.cn>
2015-05-21 14:32:28 +0800

    filter usage practice

---
+++ src/chap13/BeerRequestFilter.java
@@ -0,0 +1,46 @@
+package chap13;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class BeerRequestFilter
+ */
+@WebFilter("/BeerRequestFilter")
+public class BeerRequestFilter implements Filter {
+
+	private FilterConfig fc;
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// do clean stuff
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		System.out.println("enter doFilter.....");
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.fc = fConfig;
+	}
+
+}
